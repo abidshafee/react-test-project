aa85712d1d774271792949456420697a05e10ff4
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-10-19 18:51:29 +0600

    init cmt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const OMDbAPI = "http://www.omdbapi.com/?i=tt3896198&apikey=262924cf";
+SearchIcon = "./search-logo.svg"
+
+const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${OMDbAPI}&s=${title}`);
+        const data = await response.json();
+    }
+
+    useEffect(() => {}, []);
+    return (
+        <div>
+            <h1>My Test React App</h1>
+
+            <div className="search">
+                <input 
+                placeholder="Search Here"
+                value={"Search Value"}
+                onChange={()=>{}}/>
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                />
+
+            </div>
+        </div>
+    );
+};
 
 export default App;
